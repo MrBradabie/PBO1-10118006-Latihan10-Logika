a7d0ade3ede4c5335365f5070b4677e884e8ef0c
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-09-26 16:51:58 +0700

    Add files via upload

---
+++ PBO1-10118006-Latihan10-Logika/src/pbo1/pkg10118006/latihan10/logika/PBO110118006Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118006.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Alfarizi Rizki Pane
+ * KELAS : PBO1
+ * NIM   : 10118006
+ * Deskripsi Program : Program ini berisi  program untuk logika
+ */
+public class PBO110118006Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
